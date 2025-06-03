3c0156d20a7cf7f2162c38f1cc38dd15ffa29cf4
Geno Jayadi <jayadi@amedomain.hia.rwth-aachen.de>
2025-06-03 13:25:41 +0200

    fixed plots: surrogate model comparison and pairplot of data reduced. Simplified preprocessing and fixed the location of the saved data

--- .venv/Lib/site-packages/chaospy/distributions/sampler/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Collection of variance reduction techniques."""
-from .generator import generate_samples
-
-from .sequences import *  # pylint: disable=wildcard-import
-from .latin_hypercube import create_latin_hypercube_samples
-from .antithetic import create_antithetic_variates
--- .venv/Lib/site-packages/chaospy/distributions/sampler/sequences/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Collection of low-discrepancy sequences."""
-from .additive_recursion import create_additive_recursion_samples
-from .chebyshev import create_chebyshev_samples, create_nested_chebyshev_samples
-from .grid import create_grid_samples, create_nested_grid_samples
-from .halton import create_halton_samples
-from .hammersley import create_hammersley_samples
-from .sobol import create_sobol_samples
-from .korobov import create_korobov_samples
